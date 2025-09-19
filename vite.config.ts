93d7c6c823f6c4fd92f06e89eb6a58b7993439aa
zer0p01nt <1027.bmy@gmail.com>
2025-09-20 04:26:58 +0900

    🔧 Chore: Vite 절대경로 간소화

--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,6 @@ import { resolve } from "path";
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [
-      { find: "@", replacement: resolve(__dirname, "src") },
-      {
-        find: "@components",
-        replacement: resolve(__dirname, "src/components"),
-      },
-      { find: "@types", replacement: resolve(__dirname, "src/types") },
-      { find: "@assets", replacement: resolve(__dirname, "src/assets") },
-      { find: "@styles", replacement: resolve(__dirname, "src/styles") },
-      { find: "@pages", replacement: resolve(__dirname, "src/pages") },
-    ],
+    alias: [{ find: "@", replacement: resolve(__dirname, "src") }],
   },
 });
