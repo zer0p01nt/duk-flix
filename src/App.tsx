5fa565f56dbef70c41f26ec01163d61b325c18ba
zer0p01nt <1027.bmy@gmail.com>
2025-09-21 21:24:24 +0900

    ➕ Deps: Tanstack Query 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,25 @@ import GlobalStyle from "@/styles/GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme";
 import { Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Welcome from "./pages/Welcome/Welcome";
 import Login from "./pages/Login/Login";
 
+const qc = new QueryClient();
+
 function App(): React.JSX.Element {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Routes>
-        <Route index element={<Welcome />} />
-        <Route path='/login' element={<Login />} />
-      </Routes>
-    </ThemeProvider>
+    <QueryClientProvider client={qc}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Routes>
+          <Route index element={<Welcome />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </ThemeProvider>
+      {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
+    </QueryClientProvider>
   );
 }
 
