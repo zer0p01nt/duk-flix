9772b95dd6e279d0ae480780db171ddfa520a8cf
zer0p01nt <1027.bmy@gmail.com>
2025-09-26 02:20:20 +0900

    🎨 Design: 비밀번호 재설정 페이지 UI 일부 구현

---
+++ src/pages/PasswordReset/PasswordResetStyle.ts
@@ -0,0 +1,82 @@
+import { theme } from "@/styles/theme";
+import styled from "styled-components";
+import background from "@/assets/password-reset-background.jpg";
+
+export const Background = styled.div`
+  width: 100%;
+  height: 680px;
+  background-image: url(${background});
+  background-size: cover;
+  min-height: 31rem;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  color: ${theme.color.black};
+  position: relative;
+`;
+
+export const ResetContainer = styled.div`
+  width: 100%;
+  max-width: 480px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: rgb(242, 242, 242);
+  margin: 2.5rem 2rem 3rem;
+  padding: 2.5rem;
+  gap: 1rem;
+`;
+
+export const Title = styled.div`
+  font-size: 2rem;
+  font-weight: 700;
+  align-self: flex-start;
+`;
+
+export const Text = styled.div`
+  align-self: flex-start;
+  font-size: 1rem;
+`;
+
+export const ErrorBox = styled.div`
+  background-color: rgb(247, 224, 181);
+  color: ${theme.color.black};
+  width: 100%;
+  padding: 1rem;
+  border-radius: 4px;
+  font-size: 1rem;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
+
+export const InputBox = styled.div`
+  width: 100%;
+  background-color: ${theme.color.white};
+`;
+
+export const Btn = styled.button`
+  width: 100%;
+  background-color: ${theme.color.primary};
+  color: ${theme.color.white};
+  padding: 0.75rem 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 4px;
+  border: none;
+  font-size: 1.125rem;
+  font-weight: 500;
+  transition: background-color 0.25s cubic-bezier(0.4, 0, 0.68, 0.06);
+
+  &:hover {
+    background-color: ${theme.color.primaryHover};
+  }
+`;
